8973a9169a7b41a61d647f7e46d0f10ef12bb2d7
EgorBolotow <71177475+EgorBolotow@users.noreply.github.com>
2020-09-12 22:33:30 +0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main(int argc, char**argv)
 {
-    printf("Hello world!\n");
+    float a,b,c,d,x1,x2;
+    printf("Input value a: ");
+    scanf("%f", &a);
+    printf("Input value b: ");
+    scanf("%f", &b);
+    printf("Input value c: ");
+    scanf("%f", &c);
+    d = b*b-4*a*c;
+    if (d>0) {
+        x1 = (-b + sqrt(d))/2*a;
+        x2 = (-b - sqrt(d))/2*a;
+        float dec1,dec2;
+        float frac1 = modf(x1,&dec1);
+        float frac2 = modf(x2,&dec2);
+        if (fabs(frac1)<0.01 && fabs(frac2)<0.01) {
+            printf("x1 = %d \nx2 = %d",(int)dec1,(int)dec2);
+        } else {
+            printf("x1 = %.2f \nx2 = %.2f",x1,x2);
+        }
+    } else if (d=0) {
+        x1 = (-b + sqrt(d))/2;
+        printf("x1 = %.2f\n",x1);
+    } else {
+
+    }
     return 0;
 }
