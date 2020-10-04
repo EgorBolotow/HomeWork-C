bb22c1a10335fdc76cb8580f489f4e916486da1c
EgorBolotow <71177475+EgorBolotow@users.noreply.github.com>
2020-10-04 23:25:45 +0700

    04.10.2020 Version

--- LAB6.c
+++ LAB6.c
@@ -1,12 +1,57 @@
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
 
-int isLeapYear (int year) {
+unsigned char leapYearMonths [12] = {
+        31, // January
+        29, // February
+        31, // March
+        30, // April
+        31, // May
+        30, // June
+        31, // July
+        31, // August
+        30, // September
+        31, // October
+        30, // November
+        31  // December
+};
+
+unsigned char commonYearMonths [12] = {
+        31, // January
+        28, // February
+        31, // March
+        30, // April
+        31, // May
+        30, // June
+        31, // July
+        31, // August
+        30, // September
+        31, // October
+        30, // November
+        31  // December
+};
+
+//                Функция проверяет введенные числа на корректность
+
+void checkValues ( unsigned d1, unsigned d2, unsigned m1, unsigned m2, unsigned y1, unsigned y2) {
+
+    if (d1<=0 || d2<=0 || m1<=0 || m2<=0 || y1<=0 || y2<=0) {
+        printf("Вы ввели отрицательные числа или нулевые числа!");
+        exit(0);
+
+    }
+
+    if (isLeap)
+}
+
+//                Функция пороверяет является ли год високосным
+
+char isLeapYear (unsigned int year) {
 
     if (year%4!=0) {
         return 0;
     } else {
-        if (y%100 == 0 && y%400 != 0) {
+        if (year%100 == 0 && year%400 != 0) {
             return 0;
         } else {
             return 1;
@@ -14,85 +59,26 @@ int isLeapYear (int year) {
     }
 }
 
+int numberDaysinYear (year) {
+
+}
+
+
 int main () {
 
-    int day1;
-    int day2;
-    int month1;
-    int month2;
-    int year1;
-    int year2;
+    unsigned int day1;
+    unsigned int day2;
+    unsigned int month1;
+    unsigned int month2;
+    unsigned int year1;
+    unsigned int year2;
 
     printf("Введите первую дату: ");
     scanf ("%d %d %d", &day1, &month1, &year1 );
     printf("Введите вторую дату: ");
     scanf ("%d %d %d", &day2, &month2, &year2);
 
-    if (isLeapYear(year1)) {
-        unsigned char leapMonths1 [12] = {
-                31, // January
-                29, // February
-                31, // March
-                30, // April
-                31, // May
-                30, // June
-                31, // July
-                31, // August
-                30, // September
-                31, // October
-                30, // November
-                31  // December
-        } else {
-            unsigned char commonMonths1 [12] = {
-                    31, // January
-                    28, // February
-                    31, // March
-                    30, // April
-                    31, // May
-                    30, // June
-                    31, // July
-                    31, // August
-                    30, // September
-                    31, // October
-                    30, // November
-                    31  // December
-            }
-        }
-    }
+    checkValues(day1, day2, month1, month2, year1, year2);
 
-    if (isLeapYear(year2)) {
-        unsigned char leapMonths2 [12] = {
-                31, // January
-                29, // February
-                31, // March
-                30, // April
-                31, // May
-                30, // June
-                31, // July
-                31, // August
-                30, // September
-                31, // October
-                30, // November
-                31  // December
-        } else {
-            unsigned char commonMonths2 [12] = {
-                    31, // January
-                    28, // February
-                    31, // March
-                    30, // April
-                    31, // May
-                    30, // June
-                    31, // July
-                    31, // August
-                    30, // September
-                    31, // October
-                    30, // November
-                    31  // December
-            }
-        }
-
-
-
-    
     return 0;
 }
