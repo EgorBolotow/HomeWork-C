ec72564c1da3d6017bbf25bf18903ee2666fbdf4
EgorBolotow <71177475+EgorBolotow@users.noreply.github.com>
2020-10-06 09:57:28 +0700

    06 10 2020 V
    Осталось внести корректировки

--- LAB6.c
+++ LAB6.c
@@ -43,7 +43,121 @@ char isLeapYear (unsigned int year) {
         } else {
             return 1;
         }
+    }                printf("Вы ввели некорректное значение!");
+74
+                exit(0);
+75
+            }
+76
+        }
+77
+    } else {
+78
+        if (m1==2) {
+79
+            if (d1>28) {
+80
+                printf("Вы ввели некорректное значение!");
+81
+                exit(0);
+82
+            }
+83
+        }
+84
     }
+85
+​
+86
+    if (isLeapYear(y2)) {
+87
+        if (m2==2) {
+88
+            if (d2>=29) {
+89
+                printf("Вы ввели некорректное значение!");
+90
+                exit(0);
+91
+            }
+92
+        }
+93
+​
+94
+    } else {
+95
+        if (m2==2) {
+96
+            if (d2>=28) {
+97
+                printf("Вы ввели некорректное значение!");
+98
+                exit(0);
+99
+            }
+100
+        }
+101
+    }
+102
+}
+103
+​
+104
+​
+105
+int numberDaysinYear (year) {
+106
+​
+107
+}
+108
+​
+109
+​
+110
+int main () {
+111
+​
+112
+    unsigned int day1;
+113
+    unsigned int day2;
+114
+    unsigned int month1;
+115
+    unsigned int month2;
+116
+    unsigned int year1;
+117
+    unsigned int year2;
+118
+​
+119
+    printf("Введите первую дату: ");
+120
+    scanf ("%d %d %d", &day1, &month1, &year1 );
+121
+    printf("Введите вторую дату: ");
+122
+    scanf ("%d %d %d", &day2, &month2, &year2);
+123
+​
+124
+    checkValues(day1, day2, month1, month2, year1, year2);
+125
+​
+126
+​
+127
+​
+128
+    return 0;
+129
+}
+130
+
 }
 
 //                Функция проверяет введенные числа на корректность
@@ -101,13 +215,30 @@ void checkValues ( unsigned d1, unsigned d2, unsigned m1, unsigned m2, unsigned 
     }
 }
 
+int numberDaysinYear (year, month ,day) {
+    int days = 0;
+    int resultDays = 0;
+    if (isLeapYear(year) ) {
+        for (int i = 0; i<=month - 1 ; i++) {
+            days += leapYearMonths[i];
+        }
+        days += day;
+        resultDays = 366 - days;
+        return resultDays;
+    }
 
-int numberDaysinYear (year) {
-
+    else {
+        for (int i = 0; i<=month - 1 ; i++) {
+            days += commonYearMonths[i];
+        }
+        days += day;
+        resultDays = 365 - days;
+        return resultDays;
+    }
 }
 
 
-int main () {
+int main (int argc, char **argv) {
 
     unsigned int day1;
     unsigned int day2;
@@ -123,7 +254,39 @@ int main () {
 
     checkValues(day1, day2, month1, month2, year1, year2);
 
+    unsigned int result = 0;
 
+    if (year1 > year2) {
+        for (int j = year2; j<=year1; j++) {
+            if (j == year2) {
+                result += numberDaysinYear(year2, month2, day2);
+            } else if (j == year1) {
+                result += numberDaysinYear(year1, month1, day1);
+            } else {
+                if (isLeapYear(j)) {
+                    result += 366;
+                } else {
+                    result += 365;
+                }
+            }
+        }
+    } else {
+        for (int j = year1; j<=year2; j++) {
+            if (j == year1) {
+                result += numberDaysinYear(year1, month1, day1);
+            } else if (j == year2) {
+                result += numberDaysinYear(year2, month2, day2);
+            } else {
+                if (isLeapYear(j)) {
+                    result += 366;
+                } else {
+                    result += 365;
+                }
+            }
+        }
+    }
+
+    printf("%d", result);
 
     return 0;
 }
