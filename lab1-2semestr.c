011d7ccb662bf6c2f39e4062f5cec9f7902cc900
EgorBolotow <71177475+EgorBolotow@users.noreply.github.com>
2021-02-18 23:14:59 +0700

    Create lab1-2semestr.c

---
+++ lab1-2semestr.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "math.h"
+
+
+int sum = 0;
+int count = 0;
+const double e = 0.0001;
+double x = 0.7;
+
+int factorial (int n)
+{
+  if (n == 0 || n == 1) {
+    return 1;
+} else {
+    return n * factorial(n - 1);
+  }
+}
+
+double taylor_series (double x, int n) {
+
+  double buffer = (pow(x,2*n))/ (factorial(2*n));
+    if (fabs(buffer) > e) {
+      return buffer + taylor_series(x,n+1);
+    }
+
+}
+
+int main()
+{
+
+  printf ("%.4f",taylor_series(x,0));
+  return 0;
+}
