aa3448d257a0483fac6dcbaaaa9e0225201cfdc1
EgorBolotow <71177475+EgorBolotow@users.noreply.github.com>
2021-03-30 15:47:00 +0700

    Update lab4-saod.c

--- lab4-saod.c
+++ lab4-saod.c
@@ -3,7 +3,7 @@
 
 
 typedef struct node {
-    void *value;
+    int value;
     struct node *next;
     struct node *prev; 
 } Node;
@@ -25,12 +25,12 @@ List* create_List() {
 }
 
 
-void pushFront(List* list, void *data) {
+void pushFront(List* list, int* data) {
     Node *temp = (Node*)malloc(sizeof(Node));
     if (temp == NULL) {
         exit(1);
     }
-    temp->value = data;
+    temp->value = *(data);
     temp->next = list->head;
     temp->prev = NULL;
     if (list->head) {
@@ -45,12 +45,12 @@ void pushFront(List* list, void *data) {
 }
 
 
-void pushBack(List* list, void *value) {
+void pushBack(List* list, int* value) {
     Node *temp = (Node*)malloc(sizeof(Node));
     if (temp == NULL) {
         exit(3);
     }
-    temp->value = value;
+    temp->value = *(value);
     temp->next = NULL;
     temp->prev = list->tail;
     if (list->tail) {
@@ -65,25 +65,123 @@ void pushBack(List* list, void *value) {
 }
 
 
-void print_Int(void *value) {
-    printf("%d ", *((int*) value));
+void print_Int(int value) {
+    printf("%d ", value);
 }
 
 
-void print_List(List* list, void (*fun)(void*)) {
+void print_List(List* list) {
     Node *temp = list->head;
     while (temp) {
-        fun(temp->value);
+        printf("%d ", temp->value);
         temp = temp->next;
     }
     printf("\n");
 }
 
-void* k_em_back (List* list, int k) {
-    for (k;k!=0;k--) {
-        list = list->tail->prev;    
+
+Node* getNode(List *list, size_t index) {
+    Node *temp = list->head;
+    size_t i = 0;
+ 
+    while (temp && i < index) {
+        temp = temp->next;
+        i++;
+    }
+ 
+    return temp;
+}
+
+void deleteNode(List *list, size_t index) {
+    Node *elm = NULL;
+    elm = getNode(list, index);
+    if (elm == NULL) {
+        exit(5);
+    }
+    if (elm->prev) {
+        elm->prev->next = elm->next;
+    }
+    if (elm->next) {
+        elm->next->prev = elm->prev;
+    }
+ 
+    if (!elm->prev) {
+        list->head = elm->next;
+    }
+    if (!elm->next) {
+        list->tail = elm->prev;
+    }
+ 
+    free(elm);
+ 
+    list->size--;
+ 
+}
+
+void insertNode(List *list, size_t index, int* value) {
+    Node *elm = NULL;
+    Node *ins = NULL;
+    elm = getNode(list, index);
+    if (elm == NULL) {
+        exit(5);
+    }
+    ins = (Node*) malloc(sizeof(Node));
+    ins->value = *(value);
+    ins->prev = elm;
+    ins->next = elm->next;
+    if (elm->next) {
+        elm->next->prev = ins;
+    }
+    elm->next = ins;
+ 
+    if (!elm->prev) {
+        list->head = elm;
+    }
+    if (!elm->next) {
+        list->tail = elm;
+    }
+ 
+    list->size++;
+}
+
+int k_em_back (List* list, int k) {
+    Node* temp = list->tail;
+    for (k;k>1;k--) {
+        temp = temp->prev;    
     }   
-    return list->tail->value;
+    return temp->value;
+}
+
+void deleteDuplicates (List* list) {
+    Node* temp = list->head;
+    for (int i = 0; i < list->size-1; i++) {
+        if (temp->value == temp->next->value) {
+            deleteNode(list, i+1);
+            temp = temp->next;
+        }   
+    }
+}
+
+int isPalindrom (List* list) {
+    int flag = 1;
+    Node* t_head = list->head;
+    Node* t_tail = list->tail;
+    
+    while (flag) {
+        if (t_tail != t_head) {
+            if (t_tail->value == t_head->value) {
+                flag = 1;
+            } else {
+                flag = 0;
+            }
+        } else {
+            break;
+        }
+        t_head = t_head->next;
+        t_tail = t_tail->prev;
+    }
+    
+    return flag;
 }
 
 int main(void)
@@ -91,16 +189,40 @@ int main(void)
     int a = 10;
     int b = 20;
     int c = 30;
+    int d = 5;
     List* list = create_List();
     pushFront(list,&a);
-    print_List(list,print_Int);
+    pushFront(list,&a);
     pushFront(list,&b);
-    print_List(list,print_Int);
+    pushFront(list,&b);
     pushBack(list,&c);
-    print_List(list,print_Int);
-    
-    int* val = k_em_back(list,2);
-    printf("%d", *val);
+    pushBack(list,&c);
 
+    print_List(list);
+    
+    int k = 3; int i = 1;
+    
+    printf("%d элемент с конца = %d\n", k, k_em_back(list,k));
+    
+    deleteDuplicates(list);
+    print_List(list);
+    printf("\n");
+    
+    // check Palindrom
+
+    List* list2 = create_List();
+    pushFront(list2,&a);
+    pushFront(list2,&b);
+    pushFront(list2,&b);
+    pushFront(list2,&a);
+    print_List(list2);
+    if (isPalindrom(list2)) {
+        printf("Число является палиндромом");
+    } else {
+        printf("Не является палиндромом");
+    }
+
+    free(list);
+    free(list2);
     return 0;
-}
+}  
