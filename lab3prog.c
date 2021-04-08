b4668e36fc8ddbf2d786ce61a9515ac596d2c231
EgorBolotow <71177475+EgorBolotow@users.noreply.github.com>
2021-04-08 15:28:20 +0700

    Update lab3prog.c

--- lab3prog.c
+++ lab3prog.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
 #include <stdint.h>
 
-void swap(void * a, void * b, size_t len) {
-    uint8_t * p = a, * q = b, tmp;
-    for(len /= 2; len != 0; len--) {
-        uint8_t t = *p;
-        *p = *q;
-        *q = t;
-        a++;
-        b--;
+void swap(void* p1, void* p2, size_t size) {
+    uint8_t* u1 = (uint8_t*) p1;
+    uint8_t* u2 = (uint8_t*) p2;
+    for(size_t i = 0; i != size; i++) {
+        *u1 ^= *u2;
+        *u2 ^= *u1;
+        *u1 ^= *u2;
+        u1++;
+        u2++;
     }
 }
 
 int compare (void *a, void *b) {
     if (*(int*)a>*(int*)b) {
-        return 1;
-    } else if (*(int*)a==*(int*)b){
-        return 0;
-    } else {
         return -1;
-    }   
+    } else if (*(int*)a==*(int*)b){
+        return 1;
+    } else {
+        return 1;
+    }
 }
 
 void cocktail_sort (void *array, int n,size_t size, int (*fun)(void*, void*)) {
@@ -63,9 +64,8 @@ void printArray(int a[], int n)
     printf("\n");
 }
 
-int main()
-{
-    int a[] = { 5, 1, 4, 2, 8, 0, 2 };
+int main() {
+    int a[] = { 5, 1, 4, 2, 9, 8, 10, 0 , 2 };
     int n = sizeof(a) / sizeof(a[0]);
     printArray(a,n);
     cocktail_sort(a, n, sizeof(int), compare);
